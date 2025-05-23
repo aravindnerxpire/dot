a175d8e90f8cc06bb31bedea026efdf88baf9197
anish <anishkumar15691@gmail.com>
2025-05-23 13:34:28 +0530

    i change the mango_login header

--- application/views/mango_login.php
+++ application/views/mango_login.php
@@ -6,7 +6,7 @@
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
-    <h2>Login</h2>
+    <h2>MY Login Page</h2>
 
     <?php if (!empty($error)) echo '<p style="color:red;">' . $error . '</p>'; ?>
 
